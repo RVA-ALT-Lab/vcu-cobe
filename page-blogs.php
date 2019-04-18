1719700834ab9755cbaece2384feda70bf7b70a8
Tom Woodward <twwoodward@me.com>
2019-04-18 13:05:16 -0400

    just giving up on things to make it easier
    tags removed bc duplicate issues with names
    
    future fix would be to prepend a # or something but that requires writing a custom loop for the tags or js to do it post

--- page-blogs.php
+++ page-blogs.php
@@ -52,8 +52,9 @@
 
 
                  	<?php //printf( '<p class="footer-category">' . __('filed under', 'bonestheme' ) . ': %1$s</p>' , get_the_category_list(', ') ); ?>
+				 
 
-                  <?php the_tags( '<p class="footer-tags tags"><span class="tags-title">' . __( 'Tags:', 'bonestheme' ) . '</span> ', ', ', '</p>' ); ?>
+                  <?php //the_tags( '<p class="footer-tags tags"><span class="tags-title">' . __( 'Tags:', 'bonestheme' ) . '</span> ', ', ', '</p>' ); ?>
 
 
 								</footer>
